b076ba03fe3dab68459b1bc61973f62582e8074c
Agnieszka Brown <abrown2012@icloud.com>
2021-05-19 15:44:36 -0400

    commit tree 4125b6f12fc57834b7609f18795519bbfce37052

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+const findMatching = (drivers, name) => {
+    return drivers.filter(d => {
+        return d.toLowerCase() === name.toLowerCase()
+})
+
+}
+
+const fuzzyMatch = (drivers, start) => {
+    return drivers.filter(d => {
+        return d.toLowerCase().startsWith(start.toLowerCase())
+    })
+}
+
+const matchName = (drivers, name) => {
+    return drivers.filter(d => {
+        return d.name.toLowerCase() === name.toLowerCase()
+})
+
+}
